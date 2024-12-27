bf71c94b2392ee133d330fe650411f413f1ddc28
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-12-27 17:09:52 -0500

    Passes all tests FINALLY

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9\s'-]/g, "")
+  }
+  static titleize(string) {
+    const staticWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newString = string[0].toUpperCase() + string.slice(1);
+    let words = newString.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (!staticWords.includes(words[i])) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+      }
+    }
+    return words.toString().replaceAll(',', ' ');
+  }
 }
